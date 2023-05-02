da72e33d1c92973c8943034571a0420210eb96bd
chellamuthu94 <odcchella@gmail.com>
2023-05-02 17:45:48 +0530

    test

---
+++ src/main/java/hrm/browser.java
@@ -0,0 +1,30 @@
+package hrm;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import io.cucumber.java.en.Given;
+
+public class browser {
+	static WebDriver driver;
+	@Test
+	static public void verify_the_browser_launching() {
+		driver =  new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30000, TimeUnit.SECONDS);
+		
+	
+		driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");  
+		driver.findElement(By.xpath("//*[@type='submit']")).click() ;
+
+	}
+	
+     
+}
